4b8b001130f24336d8694128e441a3fb78a60834
younhoso <thdbsgh3443@naver.com>
2021-12-13 14:53:22 +0900

    Step1 메뉴 수정하기

--- src/js/index.js
+++ src/js/index.js
@@ -7,8 +7,8 @@
 // - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 
 // TODO 메뉴 수정
-// - [] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달항이 뜬다.
-// - [] 모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
+// - [x] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
+// - [x] 모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
 
 // TODO 메뉴 삭제
 // - [] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
@@ -19,6 +19,15 @@
 const $ = element => document.querySelector(element);
 
 function App(){
+
+    $("#espresso-menu-list").addEventListener("click", (e) => {
+        if(e.target.classList.contains('menu-edit-button')) {
+            const $menuName = e.target.closest('li').querySelector('.menu-name');
+            const updateedMenuName = prompt('메뉴명을 수정하세요', $menuName.innerText);
+            $menuName.innerText = updateedMenuName;
+        }
+    });
+
     $('#espresso-menu-form').addEventListener('submit', (e) => {
         e.preventDefault();
     });
@@ -32,16 +41,10 @@ function App(){
         const menuItemTemplate = (menuName) => {
         return `<li class="menu-list-item d-flex items-center py-2">
             <span class="w-100 pl-2 menu-name">${menuName}</span>
-            <button
-                type="button"
-                class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-            >
+            <button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button">
                 수정
             </button>
-            <button
-                type="button"
-                class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-            >
+            <button type="button" class="bg-gray-50 text-gray-500 text-sm menu-remove-button">
                 삭제
             </button>
         </li>
