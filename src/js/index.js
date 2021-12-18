8a30048f14d107e42ae44f71f5c4ee01abef1fc1
younhoso <thdbsgh3443@naver.com>
2021-12-18 16:45:58 +0900

    localStorage에 메뉴 상태를 저장하기 - 추가, 수정, 삭제

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 // Step2
 // TODO localStorage Read & Write
-// - [] localStorage에 데이터를 저장한다.
+// - [x] localStorage에 데이터를 저장한다.
+//  - [x] 메뉴를 추가할 때
+//  - [x] 메뉴를 수정할 때
+//  - [x] 메뉴를 삭제할 때
 // - [] localStorage에 있는 데이터를 읽어온다.
 
 // TODO 카테고리별 메뉴판 관리
@@ -22,7 +25,19 @@
 
 const $ = element => document.querySelector(element);
 
+const store = {
+    setLocalStorage(menu) {
+        localStorage.setItem('menu', JSON.stringify(menu));
+    },
+    getLocalStorage() {
+        localStorage.getItem('menu');
+    }
+}
+
 function App(){
+    // 상태를 변하는 데이터, 이 앱에서 변하는 것이 무엇인가 - 메뉴명
+    this.menu = new Array();
+
     const updateMenuCount = () => {
         const mentCount = $('#espresso-menu-list').querySelectorAll('li').length;
         $('.menu-count').innerText = `총 ${mentCount}개`;
@@ -33,34 +48,40 @@ function App(){
             alert('값을 입력해주세요.');
             return;
         }
-        const menuName = $('#espresso-menu-name').value;
-        const menuItemTemplate = (menuName) => {
-        return `<li class="menu-list-item d-flex items-center py-2">
-            <span class="w-100 pl-2 menu-name">${menuName}</span>
-            <button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button">
-                수정
-            </button>
-            <button type="button" class="bg-gray-50 text-gray-500 text-sm menu-remove-button">
-                삭제
-            </button>
-        </li>
-        `;
-        }
+        const espressoMenuName = $('#espresso-menu-name').value;
+        this.menu.push({ name: espressoMenuName });
+        store.setLocalStorage(this.menu); //상태가 변하는 시졈에 localStorage에 데이터를 저장한다.
+        const template = this.menu.map((item, idx) => {
+            return `<li data-menu-id="${idx}" class="menu-list-item d-flex items-center py-2">
+                <span class="w-100 pl-2 menu-name">${item.name}</span>
+                <button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button">
+                    수정
+                </button>
+                <button type="button" class="bg-gray-50 text-gray-500 text-sm menu-remove-button">
+                    삭제
+                </button>
+            </li>`
+        }).join(''); //배열객체에 있는 li항목 들을 하나로 합쳐주는 join역할
 
-        $('#espresso-menu-list').insertAdjacentHTML('beforeend', menuItemTemplate(menuName));
-
+        $('#espresso-menu-list').innerHTML = template
         updateMenuCount();
         $('#espresso-menu-name').value = '';
     };
 
     const updateMenuName = (e) => {
+        const menuId = e.target.closest('li').dataset.menuId;
         const $menuName = e.target.closest('li').querySelector('.menu-name');
         const updateedMenuName = prompt('메뉴명을 수정하세요', $menuName.innerText);
+        this.menu[menuId].name = updateedMenuName; //기존에 있던 this.menu 배열에 수정하여 저장한다.
+        store.setLocalStorage(this.menu); //그리고 다시 localStorage에 데어터를 저장한다.
         $menuName.innerText = updateedMenuName;
     };
 
     const removeMenuName = (e) => {
         if(confirm('정말 삭제하시겠습니까?')){
+            const menuId = e.target.closest('li').dataset.menuId;
+            this.menu.splice(menuId, 1);
+            store.setLocalStorage(this.menu);
             e.target.closest('li').remove();
             updateMenuCount();
         }
